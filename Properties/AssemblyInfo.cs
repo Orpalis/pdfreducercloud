c07096f3a7bfbbe39b484bdd94681141cd57e10a
bzglm <evan.carrere@epitech.eu>
2018-04-11 13:00:55 -0700

    Upgrade assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Windows.Forms;
 //  You can specify all the values or you can default the Build and Revision Numbers 
 //  by using the '*' as shown below:
 //  <Assembly: AssemblyVersion("1.0.*")> 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 
 
